d4451c3da2bc1604d45be2766f74c469a16bb4f1
Victor Turitzin <turitzinv@gmail.com>
2022-07-28 20:05:19 -0400

    commit tree b40652d8a399a83c9b9011465bf47d6b5630e7bb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  #def index
+    #cookies[:page_views] ||= 0
+    #cookies[:page_views] = cookies[:page_views].to_i + 1
+    #session[:page_views] ||= 0
+    #session[:page_views] += 1
+  #end
+
 end
